55cc4ab965197e8de0df7b921ca848e8626d693b
Aleksandr <metelskiy.al@mail.ru>
2025-02-13 20:53:17 +0300

    Added outlets

--- RandomNumber/ViewController.swift
+++ RandomNumber/ViewController.swift
@@ -9,9 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var logInButton: UIButton!
+    @IBOutlet var remindPassLabel: UILabel!
+    @IBOutlet var remindLoginLabel: UILabel!
+    @IBOutlet var passwordField: UITextField!
+    @IBOutlet var userNameField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
