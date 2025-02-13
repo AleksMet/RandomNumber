7bdca6388fcff89d16b6264f0b07b472bf9d472e
Aleksandr <metelskiy.al@mail.ru>
2025-02-13 21:30:40 +0300

    change name of view controller to main

--- RandomNumber/ViewController.swift
+++ RandomNumber/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class MainViewController: UIViewController {
 
     @IBOutlet var logInButton: UIButton!
     @IBOutlet var remindPassLabel: UILabel!
